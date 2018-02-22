224f66505b503913aa08a117fa533efb2add80d3
adarsha jha <adarshajha@live.com>
2018-02-22 16:14:07 +0530

    introduction fieldset added

--- Basic-Demo/index.php
+++ Basic-Demo/index.php
@@ -43,48 +43,70 @@
     <!-- Page Content -->
     <div class="container faucetcontainer">
         <!-- MultiStep Form -->
-<div class="row">
-    <div class="col-md-10 col-md-offset-1 ">
-        <form id="msform">
-            <!-- progressbar -->
-            <ul id="progressbar">
-                <li class="active">Get XRK</li>
-                <li>Capture Data</li>
-                <li>Retrieve Data</li>
-            </ul>
-            <!-- fieldsets -->
-            <fieldset>
-                <h2 class="fs-title">Get XRK Coins</h2>
-               
-	                <input type="button" name = "newUser" class="action-button newUser" value="New User"/>
-                <p>
-                	 <h3 class="fs-subtitle">Already Registered ? Enter your address</h3>
-                	<input type="text" name = "newUser" class=" " value="" placeholder="Registered User"/>
-                </p>
-                <input type="button" name="next" class="next action-button" value="Next"/>
-            </fieldset>
-            <fieldset>
-                <h2 class="fs-title">Record your Data </h2>
-                <h3 class="fs-subtitle">Secured Blockchain Network</h3>
-                
-                <input type="text" name="gplus" placeholder="Upload your file"/>
-                <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                <input type="button" name="next" class="next action-button" value="Next"/>
-            </fieldset>
-            <fieldset>
-                <h2 class="fs-title">Retrieve your Data</h2>
-                <h3 class="fs-subtitle">Fill in your credentials</h3>
-                <input type="text" name="email" placeholder="Email"/>
-                <input type="password" name="pass" placeholder="Password"/>
-                <input type="password" name="cpass" placeholder="Confirm Password"/>
-                <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                <input type="submit" name="submit" class="submit action-button" value="Submit"/>
-            </fieldset>
-           
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 ">
+                <form id="msform">
+                    <!-- progressbar -->
+                    <ul id="progressbar">
+                        <li class="active">Introduction</li>
+                        <li>Get XRK </li>
+                        <li>Capture Data</li>
+                        <li>Retrieve Data</li>
+                    </ul>
+                     <!-- fieldsets -->
+                    <fieldset>
+                        <h2 class="fs-title">Secure your data by uploading it on Recordskeeper </h2>
+                            <h3 class="fs-subtitle">Let's learn How to upload your confidential  data on Recordskeeper        Blockchain in just few clicks.
+                                
+                            </h3>
+<!--
+                            <p>Things you will learn here</p>
+                            <ul>
+                                <li>Get XRK Token</li>
+                                <li>Uploading Data on Blockchain</li>
+                                <li>Accessing Data stored on Blockchain</li>
+                                
+                            </ul>
+-->
+                           
 
-        </form>
-    </div>
-</div>
+                        <input type="button" name="next" class="next action-button" value="Next"/>
+                    </fieldset>
+                    <!-- fieldsets -->
+                    <fieldset>
+                        <h2 class="fs-title">Get XRK Token</h2>
+
+                            <input type="button" name = "newUser" class="action-button newUser" value="New User"/>
+                            <div>
+                                <h3 class="fs-subtitle">Already Registered ? Enter your address</h3>
+                                <input type="text" name = "newUser" class=" " value="" placeholder="Registered User"/>
+                            </div>
+                        
+                         <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
+                        <input type="button" name="next" class="next action-button" value="Next"/>
+                    </fieldset>
+                    <fieldset>
+                        <h2 class="fs-title">Record your Data </h2>
+                        <h3 class="fs-subtitle">Secured Blockchain Network</h3>
+                        <input type="text" name="idkey" placeholder="Upload your Key"/>
+                        <input type="text" name="gplus" placeholder="Upload your Data in JSON"/>
+                        <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
+                        <input type="button" name="next" class="next action-button" value="Next"/>
+                    </fieldset>
+                    <fieldset>
+                        <h2 class="fs-title">Retrieve your Data</h2>
+                        <h3 class="fs-subtitle">Fill in your credentials</h3>
+                        <input type="text" name="email" placeholder="Email"/>
+                        <input type="password" name="pass" placeholder="Password"/>
+                        <input type="password" name="cpass" placeholder="Confirm Password"/>
+                        <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
+                        <input type="submit" name="submit" class="submit action-button" value="Submit"/>
+                    </fieldset>
+
+
+                </form>
+            </div>
+        </div>
 <!-- /.MultiStep Form -->
     </div>
 
