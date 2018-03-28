acb1b0b04520d1b301ef863d6834f9ba994c2c81
Adarsha Jha <adarshajha@live.com>
2018-03-28 18:26:37 +0530

    footer updated

--- basic-demo/index.php
+++ basic-demo/index.php
@@ -386,16 +386,18 @@
   </body>
 <footer id="footer">
           <ul>
-          <li> &copy; RecordsKeeper <span class="date">2018</span></li>
+          <li> &copy; 2016-2018 <span class="date">   Recordskeeper. All rights reserved </span></li>
           <li><a href="./" target="_blank">Terms</a></li>
           <li><a href="./" target="_blank">Privacy Policy</a></li>
           <li><a href="http://explorer.recordskeeper.co/" target="_blank">Mainnet Explorer</a></li>
           <li><a href="http://test-explorer.recordskeeper.co/" target="_blank">Testnet Explorer</a></li>
           <li><a href="http://faucet.recordskeeper.co/" target="_blank">Faucet</a></li>
-          <li><a href="http://stats.recordskeeper.co/" target="_blank">Stats</a></li>
-          <li><a href="http://demo.recordskeeper.co/" target="_blank">Demo</a></li>
-	   <li><a href="http://miner.recordskeeper.co/" target="_blank">Miner</a></li>
+	
+          <li><a href="http://stats.recordskeeper.co/" target="_blank">Blockchain Statistics</a></li>
+          
+	   <li><a href="http://miner.recordskeeper.co/" target="_blank">Miner Statistics</a></li>
             <li><a href="http://airdrop.recordskeeper.co/" target="_blank">Airdrop</a></li>
+	  <li><a href="http://docs.recordskeeper.co/" target="_blank">Docs</a></li>
           </ul>
 </footer>
 </html>
