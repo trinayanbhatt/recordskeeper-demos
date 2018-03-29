9206ec945fbc3da21cd47850a90274184aba424c
trinayanbhatt <trinayanbhatt1@gmail.com>
2018-03-29 19:05:57 +0530

    title changed

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="description" content="">
-	<title>Recordskeeper- AirDrop</title>
+	<title>Recordskeeper Demo</title>
 	 <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
 	 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
