8d29039aec0e5d66c6327b72f3b3a9d9456c0412
adarsha jha <adarshajha@live.com>
2018-03-29 16:37:25 +0530

    Demo main page added

---
+++ index.php
@@ -0,0 +1,120 @@
+
+<?php
+error_reporting(0);
+$config = include('./src/config.php'); 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="description" content="">
+	<title>Recordskeeper- AirDrop</title>
+	 <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
+	 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  
+  <script type= "text/javascript">
+   
+  </script>
+
+</head>
+<body> <img style="display: none;" width="80" height="80" src="assets/images/spinner.gif" id="request-spinner"> 
+
+<header >
+	<nav class="navbar navbar-default" id="navbar">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="#">
+	      	<img src="assets/images/logo.png" id="logo" class="img-responsive">
+	      </a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	     
+	    </ul>
+	  </div>
+	</nav>
+</header>
+
+
+<div class="container" id="maincont">
+	<p class="text-center heading">Welcome to RecordsKeeper Demo </p>
+	<div class="content" >
+		<!-- <form> -->
+			<div class="row  ">
+				<div id="container">
+					<div class="col-md-6">
+						<a href="/basic-demo/">
+							<div class="form-container">
+								<h2>Basic Demo</h2>
+								<p class="cardsp">
+									This Basic Demo demonstrates how data is stored, published and retrieved on Blockchain.
+								</p>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-6">
+						<a href="/proof-of-existence/">
+							<div class="form-container">
+								<h2>Proof Of Existence</h2>
+								<p class="cardsp">
+									This Basic Demo demonstrates the use case of Document Verification on Blockchain.
+								</p>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			
+			
+
+			
+
+			
+
+			
+     
+		<!-- </form> -->
+	</div>
+</div>
+
+
+<footer id="footer">
+          <ul>
+          <li>&copy; RecordsKeeper @<span class="date">2016-2018. All rights reserved</span></li>
+           <li><a href="./" target="_blank">Terms</a></li>
+           <li><a href="./" target="_blank">Privacy Policy</a></li>
+           <li><a href="http://explorer.recordskeeper.co/" target="_blank">Mainnet Explorer</a></li>
+           <li><a href="http://test-explorer.recordskeeper.co/" target="_blank">Testnet Explorer</a></li>
+           <li><a href="http://wallet.recordskeeper.co/" target="_blank">Wallet</a></li>
+           <li><a href="http://faucet.recordskeeper.co/" target="_blank">Faucet</a></li>
+           <li><a href="http://stats.recordskeeper.co/" target="_blank">Blockchain Statistics</a></li>
+           <li><a href="http://miner.recordskeeper.co/" target="_blank">Miner Statistics</a></li>
+           
+           <li><a href="http://demo.recordskeeper.co/" target="_blank">Demo</a></li>
+	   <li><a href="https://docs.recordskeeper.co/" target="_blank">Docs</a></li>
+
+          </ul>
+</footer>
+
+
+</div>      
+      
+<!-- all the script here to make the page load faster -->
+
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+   
+<script src="assets/js/main.js?v=0.1.0"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.0/sweetalert2.all.min.js"></script>
+
+
+</body>
+</html>
